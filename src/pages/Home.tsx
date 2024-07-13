a84248ef812f354a430fd111ee293cb8a4007062
Jigyansu Nanda <nandajigyansu@gmail.com>
2024-07-13 06:02:23 +0530

    all changes

---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+export const Home = () => {
+    return (
+        <section id="me" className="content-section">
+            <h1>jigyansu</h1>
+            <p>
+                i am jigyansu nanda. you can call me <i>jig</i>.
+            </p>
+            <p>
+                i'm a fullstack software engineer, with leanings towards backend
+            </p>
+            <p>
+                formerly part of the founding team at&nbsp;
+                <a href="#">chef on wheelz</a>, where i built complete backend
+                and frontend
+            </p>
+            <p>
+                this website will serve as a place to dump all my thoughts, or
+                at least the ones i deem worthy of writing down
+            </p>
+        </section>
+    );
+};
---
+++ src/types/Constant.types.ts
@@ -0,0 +1,24 @@
+export const programmingLanguages: ReadonlyArray<string> = [
+    "Java",
+    "JavaScript",
+    "TypeScript",
+    "C++",
+];
+
+export const frameworksAndLibraries: ReadonlyArray<string> = [
+    "Spring Framework",
+    "Spring Boot",
+    "Node.js",
+    "Express.js",
+    "React.js",
+    "jQuery",
+    "Redux",
+];
+
+export const database: ReadonlyArray<string> = ["MongoDB", "SQL", "Redis"];
+
+export const cloudServies: ReadonlyArray<string> = [
+    "AWS EC2",
+    "AWS S3",
+    "AWS Route 53",
+];
